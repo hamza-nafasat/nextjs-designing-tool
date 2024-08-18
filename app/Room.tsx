0bbaed93b6a970e7ceadd24a775cb0877faadceb
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-08-18 17:58:38 +0500

    init project with liveblocks

---
+++ app/Room.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { ClientSideSuspense, LiveblocksProvider, RoomProvider } from "@liveblocks/react";
+import { ReactNode } from "react";
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider publicApiKey={process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY as string}>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>{children}</ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
